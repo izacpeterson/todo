a8c0fd3373e958ee4747503c649b8510fbfa7a2e
izacpeterson <izacpeterson@gmail.com>
2021-09-23 14:08:24 -0600

    worked on DOM and event functions

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 /** @format */
-
+//Classes and methods
+class app {
+  constructor(user, lists) {
+    this.user = user;
+    this.lists = [];
+  }
+  addList(listName) {
+    let array = this.lists;
+    array.push(listName);
+    this.lists = array;
+  }
+  save() {
+    localStorage.setItem("todoApp", this);
+  }
+}
 class toDoList {
   constructor(listName, tasks) {
     this.listName = listName;
@@ -29,16 +43,52 @@ class task {
     }
   }
 }
-let list = new toDoList("list");
-console.log(list);
-let task1 = new task("task1");
-let task2 = new task("task2");
-let task3 = new task("task3");
-let task4 = new task("task4");
+//Declare App
+let myApp = new app();
 
-list.addTask(task1);
-list.addTask(task2);
-list.addTask(task3);
-list.addTask(task4);
+//New List button and event listener
+let newListButton = document.getElementById("newListButton");
+newListButton.addEventListener("click", () => {
+  addList(document.getElementById("newListName").value);
+});
 
-console.log(list);
+function addList(listName) {
+  myApp.addList(new toDoList(listName));
+  updateListDOM(listName);
+}
+function updateListDOM() {
+  let dropdownList = document.getElementById("dropdownList");
+  let listH2 = document.getElementById("listName");
+  listH2.innerText = myApp.lists[0].listName;
+
+  dropdownList.innerHTML = "";
+  myApp.lists.forEach((i) => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(i.listName));
+    li.setAttribute("class", "dropdown-item");
+    dropdownList.appendChild(li);
+  });
+}
+function addTask(listIndex, taskName) {
+  myApp.lists[listIndex].addTask("taskName");
+}
+function updateTaskDOM(listIndex) {
+  myApp.lists[listIndex].tasks.forEach((t) => {
+    let taskList = document.getElementById("taskList");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(t.taskName));
+    console.log(t);
+    li.setAttribute("class", "list-group-item");
+    taskList.appendChild(li);
+  });
+}
+function tester() {
+  myApp.addList(new toDoList("myList"));
+  myApp.lists[0].addTask(new task("Wake Up"));
+  myApp.lists[0].addTask(new task("Go to school"));
+  myApp.lists[0].addTask(new task("Lunch"));
+  myApp.lists[0].addTask(new task("Go to work"));
+  updateListDOM();
+  updateTaskDOM(0);
+}
+tester();
